044f480279359c50320c280f58a884abd71fee00
Lalit Mohan Pant <lalitmohanpant@Mac.lan>
2026-08-11 12:20:11 +0530

    Add recursion practice

---
+++ Recursion /name.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void printName(int i, int n) {
+    if (i > n) {
+        return;
+    }
+
+    cout << "Lalit" << endl;
+    printName(i + 1, n);
+}
+
+int main() {
+    int n ;
+    cout<<"enter the number:";
+    cin>>n;
+
+    printName(1, n);
+
+    return 0;
+}
